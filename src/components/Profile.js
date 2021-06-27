cde0ce2ba92a5d647c9af73a800083d8b1bae970
Alexandra Gritsenko <79204776+Sashikbear@users.noreply.github.com>
2021-06-27 16:39:07 +0300

    adding redirect

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,17 @@
 import "../App.css";
 import { useEffect, useState, useContext } from "react";
-import Navbar from "./Navbar";
 import AppContext from "../context/AppContext";
+import { Redirect } from "react-router-dom";
 function Profile() {
 	const storedUserName = JSON.parse(window.localStorage.getItem("username"));
 	const [userName, setUserName] = useState(storedUserName);
 	const [isDisabled, setIsDisabled] = useState(false);
 	const appContext = useContext(AppContext);
+	const [redirect, setRedirect] = useState(false);
 	const handleProfileNameChange = (event) => {
 		setUserName(event.target.value);
 	};
+
 	useEffect(() => {
 		if (userName) {
 			if (userName.length > 0) {
@@ -19,15 +21,18 @@ function Profile() {
 			}
 		}
 	}, [userName]);
+
 	const handleProfileNameSubmit = (event) => {
 		appContext.setProfileUserName(userName);
 		window.localStorage.setItem("username", JSON.stringify(userName));
 		window.alert("User name has been succesfully changed to " + userName + "!");
+		setRedirect(true);
 	};
 
 	return (
 		<div>
 			<div className="profile-wrapper">
+				{redirect && <Redirect to="/" />}
 				<h1>Profile</h1>
 				<div>User Name</div>
 				<input
